11b37a7615b7af7b51fce346623bcb096d16483a
Alejandro Ruiz <demiian88@gmail.com>
2019-10-15 12:40:03 +0200

    Version 2

--- JuegoMemoria/SecondVC.swift
+++ JuegoMemoria/SecondVC.swift
@@ -6,24 +6,37 @@
 //  Copyright © 2019 alumnos. All rights reserved.
 //
 
+var images: [UIImage] = [#imageLiteral(resourceName: "Simpson2"),#imageLiteral(resourceName: "Simpson1"),#imageLiteral(resourceName: "Simpson5"),#imageLiteral(resourceName: "Simpson4"),#imageLiteral(resourceName: "Simpson6"),#imageLiteral(resourceName: "Simpson3")]
 
+var numbers: [Int] = []
 import UIKit
 
 class SecondVC: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    var imagenes: [UIImage] = [#imageLiteral(resourceName: "Simpson2"),#imageLiteral(resourceName: "Simpson1"),#imageLiteral(resourceName: "Simpson5"),#imageLiteral(resourceName: "Simpson4"),#imageLiteral(resourceName: "Simpson6"),#imageLiteral(resourceName: "Simpson3")]
+    
     
     override func viewDidLoad() {
         changeImage()
         super.viewDidLoad()
     }
     
+    
+    func getArrayNumbers() -> [Int] {
+        var numeros: [Int] = []
+        for i in 0...images.count-1{
+            numeros.append(i)
+        }
+        numeros.shuffle()
+        
+        return numeros
+    }
     func changeImage()  {
+        numbers = getArrayNumbers()
         var index = 0
         Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true, block: { timer in
-            if index < self.imagenes.count{
-                self.imageView.image = self.imagenes[index]
+            if index < images.count{
+                self.imageView.image = images[numbers[index]]
             }
             index += 1
         })
--- JuegoMemoria/ThirdVC.swift
+++ JuegoMemoria/ThirdVC.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ThirdVC: UIViewController {
     
+    @IBOutlet weak var botonPrueba: UIButton!
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBOutlet weak var Ejemplo: UIImageView!
-    
+    func fill() {
+        var index = 0
+        image1.image = images[0]
+        index += 1
+        
+    }
     @IBAction func buttonBack(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
